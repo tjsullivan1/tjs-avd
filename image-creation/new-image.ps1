a2df949336bc629c3e19cc903b4905f799d7d937
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-06-09 22:53:38 -0500

    Update new-image.ps1

--- image-creation/new-image.ps1
+++ image-creation/new-image.ps1
@@ -1,6 +1,6 @@
 $imageResourceGroup = 'rg-images'
 $location = 'eastus'
-$templateFilePath = './image-template.json'
+$templateFilePath = './image-creation/image-template.json'
 $imageTemplateName = "se-wvd-" + (Get-Date -Format yyyyMMddHHmm)
 
 New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2020-02-14" -imageTemplateName $imageTemplateName -svclocation $location
