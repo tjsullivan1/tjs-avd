b3ac88b57ef6322d76cd4768f668a5eba0544f59
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-06-09 23:08:03 -0500

    Update new-image.ps1
    using az cli instead of powershell

--- image-creation/new-image.ps1
+++ image-creation/new-image.ps1
@@ -4,4 +4,6 @@ $templateFilePath = './image-creation/image-template.json'
 $imageTemplateName = "se-wvd-" + (Get-Date -Format yyyyMMddHHmm)
 
 New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2020-02-14" -imageTemplateName $imageTemplateName -svclocation $location
-Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName  -NoWait
+
+az image builder run --name $imageTemplateName --resource-group $imageResourceGroup --no-wait
+# Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName  -NoWait
