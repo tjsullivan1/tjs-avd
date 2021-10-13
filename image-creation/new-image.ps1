935f3947078cd3bda0d669427b985cc6539be6cd
Tim Sullivan (CSA) <tisulliv@microsoft.com>
2021-10-13 11:38:16 -0500

    updating to include proper image definition for windows 11

--- image-creation/new-image.ps1
+++ image-creation/new-image.ps1
@@ -3,14 +3,26 @@ param (
     $imageResourceGroup = 'rg-images',
     $location = 'eastus',
     $templateFilePath = './image-creation/image-template.json',
-    $imageTemplateName = "se-wvd-" + (Get-Date -Format yyyyMMddHHmm)     
+    $imageTemplateName = "se-wvd-" + (Get-Date -Format yyyyMMddHHmm),
+    $sigId
 )
 
 # New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2020-02-14" -imageTemplateName $imageTemplateName -svclocation $location
-$params = @{
-    imageTemplateName = $imageTemplateName
-    svclocation = $location
+
+
+if ($sigId) {
+    $params = @{
+        imageTemplateName = $imageTemplateName
+        svclocation = $location
+        sigId = $sigId
+    }
+} else {
+    $params = @{
+        imageTemplateName = $imageTemplateName
+        svclocation = $location
+    }
 }
+
 New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -TemplateParameterObject $params
 
 az image builder run --name $imageTemplateName --resource-group $imageResourceGroup --no-wait
